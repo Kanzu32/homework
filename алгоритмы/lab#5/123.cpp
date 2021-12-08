cb9acc96c1157171ba88a68dd3ffd5e548c22133
Kanzu <kanzu32@gmail.com>
2021-12-08 08:20:39 +0300

    55

---
+++ алгоритмы/lab#5/123.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int arr[10][5];
+
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 5; j++) {
+      cin >> arr[i][j];
+    }
+  }
+
+  for (int i = 1; i < 10; i+=2) {
+    for (int j = 0; j < 5; j++) {
+        cout << arr[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
