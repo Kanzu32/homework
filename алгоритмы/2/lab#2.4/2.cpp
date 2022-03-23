0076601ec6101beebc72fcd292963ad331cfce61
Kanzu <kanzu32@gmail.com>
2022-03-23 18:27:49 +0300

    456

---
+++ алгоритмы/2/lab#2.4/2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	getline(cin, str);
+	int tokenStart = 0;
+	int tokenEnd = 0;
+	int len = 0;
+	string left, right;
+	bool fl;
+	string res;
+
+	while (tokenEnd != str.length()) {
+		tokenEnd = str.find(' ', tokenEnd);
+		fl = true;
+		len = tokenEnd - tokenStart;
+		left = str.substr(tokenStart, len / 2);
+		right = str.substr(tokenEnd - len / 2);
+
+		if (tokenEnd == -1)
+			tokenEnd = str.length() - 1;
+
+		for (int i = 0; i < len / 2; i++) {
+			if (left.at(i) != right.at(len / 2 - i - 1)) {
+				fl = false;
+				break;
+			}
+		}
+		if (fl && (str.substr(tokenStart, len).length() < res.length() || res.empty()) ) {
+			res = str.substr(tokenStart, len);
+		}
+		tokenEnd++;
+		tokenStart = tokenEnd;
+	}
+
+	cout << res;
+}
---
+++ алгоритмы/2/lab#2.4/3/build-3-Desktop_Qt_6_2_1_MinGW_64_bit-Debug/3_autogen/include/ui_mainwindow.h
@@ -0,0 +1,82 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.2.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QTextEdit *textEdit;
+    QTextEdit *textEdit_2;
+    QPushButton *pushButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(510, 366);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        textEdit = new QTextEdit(centralwidget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+        textEdit->setGeometry(QRect(30, 20, 441, 61));
+        textEdit_2 = new QTextEdit(centralwidget);
+        textEdit_2->setObjectName(QString::fromUtf8("textEdit_2"));
+        textEdit_2->setGeometry(QRect(30, 140, 441, 181));
+        textEdit_2->setUndoRedoEnabled(false);
+        textEdit_2->setReadOnly(true);
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(180, 90, 161, 41));
+        QFont font;
+        font.setPointSize(16);
+        pushButton->setFont(font);
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 510, 20));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "generate", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
