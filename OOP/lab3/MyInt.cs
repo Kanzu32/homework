7c1af457acea546ea3141eafdd67d365f4c36c50
Kanzu <kanzu32@gmail.com>
2023-10-05 18:54:50 +0300

    f

---
+++ OOP/lab3/MyInt.cs
@@ -0,0 +1,55 @@
+﻿
+namespace v4 {
+    public class MyInt {
+        private long data;
+        public MyInt() {
+            data = 0;
+        }
+        public MyInt(int d) {
+            data = d;
+        }
+
+        public MyInt(long d) {
+            if (d > Int32.MaxValue || d < Int32.MinValue) {
+                throw new OverflowException($"Value {d} overflows Int32");
+            }
+            data = d;
+        }
+
+        public override string ToString() {
+            return data.ToString();
+        }
+
+        public static MyInt operator + (MyInt a, MyInt b) {
+            long res = a.data + b.data;
+            if (res > Int32.MaxValue || res < Int32.MinValue) {
+                throw new OverflowException($"Overflow in operation {a.data} + {b.data}");
+            }
+            return new MyInt(res);
+        }
+
+        public static MyInt operator - (MyInt a, MyInt b) {
+            long res = a.data - b.data;
+            if (res > Int32.MaxValue || res < Int32.MinValue) {
+                throw new OverflowException($"Overflow in operation {a.data} - {b.data}");
+            }
+            return new MyInt(res);
+        }
+
+        public static MyInt operator * (MyInt a, MyInt b) {
+            long res = a.data * b.data;
+            if (res > Int32.MaxValue || res < Int32.MinValue) {
+                throw new OverflowException($"Overflow in operation {a.data} * {b.data}");
+            }
+            return new MyInt(res);
+        }
+
+        public static MyInt operator / (MyInt a, MyInt b) {
+            long res = a.data / b.data;
+            if (res > Int32.MaxValue || res < Int32.MinValue) {
+                throw new OverflowException($"Overflow in operation {a.data} / {b.data}");
+            }
+            return new MyInt(res);
+        }
+    }
+}
---
+++ OOP/lab3/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace v4 {
+    class Program {
+        public static MyInt? a;
+        public static MyInt? b;
+        static void Main(string[] args) {
+            while (true) {
+
+                Console.Write("Enter operation (+, -, *, /, exit): ");
+                string? operation = Console.ReadLine();
+
+                if (operation == "exit")
+                    break;
+
+                Console.Write("Enter A number: ");
+                string? aStr = Console.ReadLine();
+                Console.Write("Enter B number: ");
+                string? bStr = Console.ReadLine();
+            
+                a = new MyInt(long.Parse(aStr ?? "0"));
+                b = new MyInt(long.Parse(bStr ?? "0"));
+                
+                switch (operation) {
+                    case "+":
+                        Console.WriteLine(a + b);
+                        break;
+                    case "-":
+                        Console.WriteLine(a - b);
+                        break;
+                    case "*":
+                        Console.WriteLine(a * b);
+                        break;
+                    case "/":
+                        Console.WriteLine(a / b);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input.");
+                        break;
+
+                }
+            }
+        }
+    }
+}
